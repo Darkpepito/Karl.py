d788d5d75cd62d39a3fdd160b8d2a3e7f9ceb75e
Darkpepito <elouenn.fresquet@protonmail.com>
2023-04-12 15:27:30 +0200

    First public release

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 ﻿# Karl.py
-##  Usage
-Karl.py is an open-source multi-purpose discord bot, including games, auto-messaging and other.
+
+Karl.py est un bot mutifonctions (y'en a pas beaucoup mais bon) qui a une sévère envie de casser la tronche d'Adam Smith.
+
+## Commandes
+Commandes disponibles en version __**0.1c**__ :
+
+`/quote`: Donne une citation aléatoire de Karl  
+`/help`: Montre les commandes disponnibles  
+`/version`: Donne la version actuelle du bot  
+`/links`: Donne mes liens sociaux  
+
+## Vous voulez aider ?
+Si vous avez des propositions à faire, vous pouver ouvrir une nouvelle issue sur Github avec le tag "Suggestion".  
+Tous conseils seront les bienvenus !  
+
+## Détails
+
+### Library utilisée
+Karl.py utilise la library Nextcord (parce que c'est comme ça).  
+__Github__: https://github.com/nextcord  
+__Documentation__ : https://docs.nextcord.dev  
+
+### Remmerciements
+Merci à toutes les personnes qui ont participés de près ou de loin à ce projet :  
+- __**OmégaxX**__ pour m'avoir aidé à faire mes premiers pas avec Nextcord  
--- bot.py
+++ bot.py
@@ -13,11 +13,13 @@ intents.members = True
 
 bot = commands.Bot(intents=intents)
 
+TOKEN="YOUR_TOKEN"
+
 def timeget():
     global date
     date = str(datetime.datetime.now())
 
-version = "0.1b"
+version = "0.1c"
 
 @bot.event
 async def on_ready():
@@ -32,9 +34,10 @@ async def version_cmd(interaction: Interaction):
 
 @bot.slash_command(name="help", description="Affiche toutes les commandes possibles")
 async def help(interaction: Interaction):
-    help_embed = nextcord.Embed(title="__**Liste des commandes :**__", description=
+    help_embed = nextcord.Embed(title="__**Liste des commandes :**__", color=0x00dd02,description=
                                 "**`/version`** : montre la version actuelle de Karl.py\n" +
-                                "**`/quote`** : montre une citation aléatoire de Karl\n" + "\n"
+                                "**`/quote`** : montre une citation aléatoire de Karl\n" + 
+                                "**`/links`** : Affiche les liens sociaux\n" +
                                 "C'est tout pour l'instant, plus de contenu est à venir."
                                 )
     await interaction.response.send_message(embed=help_embed)
@@ -53,4 +56,14 @@ async def quote(interaction: Interaction):
     quote_embed.set_image(url="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjg1NjU0Mzk0MmEwNjRiYzk1Y2MyYWRlMDA3NGE2MTA0NzJkMjI4OCZjdD1n/mFw51RR5HkD4gYUbIx/giphy.gif")
     await interaction.response.send_message(embed=quote_embed)
 
-bot.run("MTA3ODc0MjMwOTA1MjYyMDgxMQ.GGBSuk.HxdzIdmbB_L0-Zr33BjImcHQyLXB-A5OGfIKqE")
+@bot.slash_command(name="links", description="Afiche les différents liens sociaux")
+async def links(interaction: Interaction):
+    links_embed = nextcord.Embed(title="__**Liens de Darkpepito**__", color=0x0071ff, description=
+                                 "__Steam:__ https://vu.fr/JIrz\n"+
+                                 "__Musique (pas ouf):__ https://vu.fr/Dtdc\n"+
+                                 "__Github:__ https://vu.fr/ORNM\n"+
+                                 "\n(Les liens sont raccourcis, ne vous inquiétez pas de leur tronche)"
+                                 )
+    await interaction.response.send_message(embed=links_embed)
+
+bot.run(TOKEN)
